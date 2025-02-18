8cf27251714842a69833d5b374144d27313160f8
Andrei Carvalho <andreicarvs@gmail.com>
2025-02-18 16:28:38 -0300

    Feature: implement product filtering

--- functions.php
+++ functions.php
@@ -2,12 +2,60 @@
 
 require_once __DIR__ . '/config.php';
 
+#Get orders sent through GET and validates them
+function getAndValidatesOrder() {
+    $_SESSION['orders'] = $_SESSION['orders'] ?? [];
+
+    $orderColumn = $_GET['order_by'] ?? null;
+    $orderDirection = $_GET['order'] ?? null;
+
+    if(in_array($orderColumn, ['name', 'categories', 'price', 'inventory', 'total'])){
+        if(in_array($orderDirection, ['asc', 'desc', 'none'])){
+            $_SESSION['orders'][$orderColumn] = $orderDirection;
+        }
+    }
+    return $_SESSION['orders'];
+}
+
+/*
+    Toggles the product display order between ascending,
+    descending, and no order based on user interaction.
+*/
+function getNextOrder($column) {
+    $currentOrder = $_SESSION['orders'][$column] ?? 'none';
+    if($currentOrder === 'asc'){
+        return 'desc';
+    } elseif ($currentOrder === 'desc'){
+        return 'none';
+    }
+    return 'asc';
+}
+
+#Set the icon according to the order direction
+
+function setDirectionIcon($column){
+    if(isset($_SESSION['orders'][$column])){
+        $direction = $_SESSION['orders'][$column];
+
+        if($direction === 'asc'){
+            return '<i class="bi bi-arrow-up"></i>';
+        } elseif ($direction === 'desc'){
+            return '<i class="bi bi-arrow-down"></i>';
+        }
+    } else {
+        return '';
+    }
+}
+
 #Get a filter from a term sent by post
 function getFilterByPost() {
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['filtro'])){
-        return $_POST['filtro'];
-    } else {
-        return null;
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['filter'])){
+        $filter = trim($_POST['filter']);
+        if (!empty($filter) && strlen($filter) > 3) {
+            $_SESSION['filter'] = "%{$filter}%";
+        } else {
+            unset($_SESSION['filter']);
+        }
     }
 }
 
@@ -17,22 +65,31 @@ function getFilterByPost() {
 */
 function listProducts($filter) {
     global $pdo;
-    $query = 'SELECT * FROM products';
-    $filterParam = '';
+    $query = 'SELECT *, (inventory * price) AS total FROM products';
 
-    if (!empty($filter) && strlen($filter) > 3) {
-        $filterParam = "%{$filter}%";
-        $query .= " WHERE name LIKE :filter";
+    if (!empty($_SESSION['filter'])) {
+        $query .= " WHERE name OR categories LIKE :filter";
+    }
+    $orders = getAndValidatesOrder();
+
+    if(!empty($orders) && is_array($orders)){
+        $orderArray = [];
+        foreach ($orders as $column => $direction){
+            if ($direction !== 'none'){
+                $orderArray[] = " {$column} {$direction}";
+            }
+        }
+        if(!empty($orderArray)){
+            $query .= " ORDER BY" . implode(',', $orderArray);
+        }
     }
 
     try {
         $stmt = $pdo->prepare($query);
 
-        if (!empty($filterParam)) {
-            $stmt->bindParam(':filter', $filterParam);
-
+        if (!empty($_SESSION['filter'])) {
+            $stmt->bindValue(':filter', $_SESSION['filter']);
         }
-
         $stmt->execute();
         return $stmt->fetchAll();
     } catch (PDOException $e) {
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/functions.php';
 
+session_start();
+
 $filterByPost = getFilterByPost();
 $index = 0;
 $products = listProducts($filterByPost);
@@ -17,13 +19,14 @@ $products = listProducts($filterByPost);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Listar produtos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-2">
         <div class="col-3 mt-3">
             <form action="" method="post">
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" name="filtro" placeholder="O que procura?" aria-label="O que procura?">
+                    <input type="text" class="form-control" name="filter" placeholder="O que procura?" aria-label="O que procura?">
                     <button class="btn btn-primary" type="submit" id="button-addon2">Buscar</button>
                 </div>
             </form>
@@ -32,24 +35,29 @@ $products = listProducts($filterByPost);
             <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">Produto</th>
-                <th scope="col">Categoria</th>
-                <th scope="col">Preço</th>
-                <th scope="col">Estoque</th>
-                <th scope="col">Total</th>
+                <th scope="col"><a href="?order_by=name&order=<?= getNextOrder('name') ?>" class="text-decoration-none text-dark">Produto </a> <?= setDirectionIcon('name')?> </th>
+                <th scope="col"><a href="?order_by=categories&order=<?= getNextOrder('categories') ?>" class="text-decoration-none text-dark">Categoria</a> <?= setDirectionIcon('categories')?> </th>
+                <th scope="col"><a href="?order_by=price&order=<?= getNextOrder('price') ?>" class="text-decoration-none text-dark">Preço</a> <?= setDirectionIcon('price')?> </th>
+                <th scope="col"><a href="?order_by=inventory&order=<?= getNextOrder('inventory') ?>" class="text-decoration-none text-dark">Estoque</a> <?= setDirectionIcon('inventory')?> </th>
+                <th scope="col"><a href="?order_by=total&order=<?= getNextOrder('total') ?>" class="text-decoration-none text-dark">Total</a> <?= setDirectionIcon('total')?> </th>
             </tr>
             </thead>
             <tbody>
-            <?php foreach ($products as $product): ?>
-            <tr>
-                <th scope="row"><?php echo ++$index ?></th>
-                <td><?php echo $product['name']; ?></td>
-                <td><?php echo $product['categories']; ?></td>
-                <td><?php echo $product['price']; ?></td>
-                <td><?php echo $product['inventory']; ?></td>
-                <td><?php echo 'R$'. number_format($product['inventory'] * $product['price'], 2, ','); ?></td>
-            </tr>
-            <?php endforeach; ?>
+            <?php if(!empty($products) && is_array($products)) : ?>
+                <?php foreach ($products as $product): ?>
+                <tr>
+                    <th scope="row"><?php echo ++$index ?></th>
+                    <td><?= $product['name']; ?></td>
+                    <td><?= $product['categories']; ?></td>
+                    <td><?='R$ ' . number_format($product['price'], 2, ',', '.'); ?></td>
+                    <td><?= $product['inventory']; ?></td>
+                    <td><?='R$ '. number_format($product['total'],2, ',', '.'); ?></td>
+                </tr>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <td colspan="6" class="text-center">Nenhum produto encontrado.</td>
+                </tr>
+            <?php endif; ?>
             </tbody>
         </table>
     </div>
